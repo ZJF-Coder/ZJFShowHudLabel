b70d763201c1f35563b0605e87997bcbf53e6510
wwyy <505228178@qq.com>
2019-10-13 21:53:12 +0800

    暂存1

--- Example/ZJFShowHudLabel/ViewController.swift
+++ Example/ZJFShowHudLabel/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import ZJFShowHudLabel
 
 class ViewController: UIViewController {
 
@@ -15,15 +16,31 @@ class ViewController: UIViewController {
         
         self.view.backgroundColor = UIColor.white
         
-        let btn = UIButton.init(frame: CGRect.init(x: 10, y: 100, width: 40, height: 20))
+        let btn = UIButton.init(frame: CGRect.init(x: 10, y: 100, width: 60, height: 30))
         btn.backgroundColor = UIColor.red
         btn.addTarget(self, action: #selector(btnDidClick), for: UIControlEvents.touchUpInside)
         self.view.addSubview(btn)
+        
+        let btn1 = UIButton.init(frame: CGRect.init(x: 10, y: 160, width: 60, height: 30))
+        btn1.backgroundColor = UIColor.blue
+        btn1.addTarget(self, action: #selector(btn1DidClick), for: UIControlEvents.touchUpInside)
+        self.view.addSubview(btn1)
         
     }
     
     @objc func btnDidClick() {
         ZJFShowHudLabel.shareHud()?.showInfo("测试", withTopY: 0, onDuration: 3)
+    }
+    
+    @objc func btn1DidClick() {
+        let dataArr:Array<[String:String]> = [["text":"男","id":"1"],["text":"女","id":"2"]]
+        
+//        let popView = ZJFPopTableView.init(frame: self.view.bounds, dataArr: dataArr)
+        
+//        popView.successBlock = { [weak self] (text,ID) in
+//            ZJFShowHudLabel.shareHud()?.showInfo("text=\(text),id=\(ID)", withTopY: 0, onDuration: 3)
+//        }
+//        view.addSubview(popView)
     }
 
     override func didReceiveMemoryWarning() {
